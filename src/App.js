8e6f18b6c205ff9f8f9bca45a3e9ddea2531e9cf
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-21 02:49:45 +0530

    chrts added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 import Layout from "./layout";
+import ChartsPage from "./chartspage";
 export default function App() {
   return (
     <div className="App">
-      <Layout />
+      {/* <Layout /> */}
+      <ChartsPage />
     </div>
   );
 }
--- src/chartspage.js
+++ src/chartspage.js
@@ -6,19 +6,11 @@ import Drawer from "@material-ui/core/Drawer";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import IconButton from "@mui/material/IconButton";
-import InputLabel from "@mui/material/InputLabel";
-import SearchIcon from "@mui/icons-material/Search";
-import PersonIcon from "@mui/icons-material/Person";
 import Divider from "@mui/material/Divider";
 
-
+import LineRechartComponent from "./linechart";
+import PieRechartComponent from "./piechart";
 const styles = (theme) => ({
-  // The main flex container for the app's layout. Its min-height
-  // is set to `100vh` so it always fill the height of the screen.
   root: {
     display: "flex",
     minHeight: "100vh",
@@ -26,43 +18,20 @@ const styles = (theme) => ({
     position: "relative",
     overflow: "hidden",
     backgroundColor: theme.palette.background.default
-  },
-  appBar: {
-    // For full-height drawer
-    //  maxWidth: `calc(100% - ${theme.layout.drawerWidth}px)`
-    maxWidth: `calc(100% - 100px)`
-  },
-  toolbarActions: {
-    marginLeft: "auto"
-  },
-  drawer: {
-    // width: theme.layout.drawerWidth
-    width: 100
-  },
-  drawerPaper: {
-    width: "inherit"
   }
 });
 
 function ChartsPage(props) {
   const { classes } = props;
   return (
-
     <>
       <Box p={1} sx={{ bgcolor: "#FFFFFF" }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography
-              variant="body"
-              gutterBottom
-              component="div"
-              align="left"
-            >
-              Employees Directory
-            </Typography>
+            <LineRechartComponent />
             <Divider />
+            <PieRechartComponent />
           </Grid>
-         
         </Grid>
       </Box>
     </>
