8df4a3ad0c8db91b0cda7ce43ef3aa9431334d5a
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-21 10:13:48 +0530

    Update chartspage.js

--- src/chartspage.js
+++ src/chartspage.js
@@ -2,10 +2,11 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Box from "@mui/material/Box";
+import Drawer from "@material-ui/core/Drawer";
 import Grid from "@mui/material/Grid";
-// import Card from "@mui/material/Card";
-// import CardContent from "@mui/material/CardContent";
-// import Divider from "@mui/material/Divider";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Divider from "@mui/material/Divider";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
@@ -17,13 +18,20 @@ function ChartsPage(props) {
   const { classes } = props;
   return (
     <>
-      <Box sx={{ flexgrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={6}>
             <Button>
               <Paper
                 elevation={2}
                 sx={{
+                  //maxWidth: "200px",
                   //padding: 1,
                   backgroundColor: "#F5EFEF"
                 }}
@@ -32,28 +40,19 @@ function ChartsPage(props) {
               </Paper>
             </Button>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={6}>
             <Button>
               <Paper
                 elevation={2}
                 sx={{
                   //maxWidth: "200px",
-                  padding: 1,
+                  //padding: 1,
                   backgroundColor: "#F5EFEF"
                 }}
               >
-                <LineRechartComponent />
+                <PieRechartComponent />
               </Paper>
             </Button>
-          </Grid>
-        </Grid>
-      </Box>
-      <Box p={1} sx={{ bgcolor: "#FFFFFF" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <LineRechartComponent />
-
-            {/* <PieRechartComponent /> */}
           </Grid>
         </Grid>
       </Box>
