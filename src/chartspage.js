82a088ffcd501ca7281428e4e584e08f86c13f8d
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-21 09:58:13 +0530

    chart changes

--- src/chartspage.js
+++ src/chartspage.js
@@ -2,30 +2,45 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Box from "@mui/material/Box";
-import Drawer from "@material-ui/core/Drawer";
 import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Divider from "@mui/material/Divider";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import Divider from "@mui/material/Divider";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 import LineRechartComponent from "./linechart";
 import PieRechartComponent from "./piechart";
-const styles = (theme) => ({
-  root: {
-    display: "flex",
-    minHeight: "100vh",
-    zIndex: 1,
-    position: "relative",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.default
-  }
-});
+const styles = (theme) => ({});
 
 function ChartsPage(props) {
   const { classes } = props;
   return (
     <>
-      <Box p={1} sx={{ bgcolor: "#FFFFFF" }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={3}>
+            <Button>
+              <Paper
+                elevation={2}
+                sx={{
+                  //maxWidth: "200px",
+                  padding: 1,
+                  backgroundColor: "#F5EFEF"
+                }}
+              >
+                <LineRechartComponent />
+              </Paper>
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+      {/* <Box p={1} sx={{ bgcolor: "#FFFFFF" }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <LineRechartComponent />
@@ -33,10 +48,8 @@ function ChartsPage(props) {
             <PieRechartComponent />
           </Grid>
         </Grid>
-      </Box>
+      </Box> */}
     </>
-    //</Box>
-    // </div>
   );
 }
 export default withStyles(styles)(ChartsPage);
