95fcff4f136711b80c7cc2b91b6904bf499a5c74
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-22 10:53:21 +0530

    changes

--- src/chartspage.js
+++ src/chartspage.js
@@ -29,204 +29,170 @@ function ChartsPage(props) {
   };
   return (
     <>
-      {showDetails === 0 ? (
+      <Box sx={{ flexGrow: 1 }}>
         <>
-          <Box sx={{ flexGrow: 1 }}></Box>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={4}>
+              <Button onClick={() => setshowchart(1)}>
+                <Paper
+                  elevation={3}
+                  sx={{
+                    maxWidth: "600px",
+                    padding: 2,
+                    color: "#041c49",
+                    backgroundColor: "#ffffff"
+                  }}
+                >
+                  <Typography variant="body1" display="block" align="center">
+                    Project branching strategy
+                  </Typography>
+                </Paper>
+              </Button>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Button onClick={() => setshowchart(2)}>
+                <Paper
+                  elevation={3}
+                  sx={{
+                    maxWidth: "600px",
+                    padding: 2,
+                    color: "#041c49",
+                    backgroundColor: "#ffffff"
+                  }}
+                >
+                  <Typography variant="body1" display="block" align="center">
+                    Project failure rate/ success rate
+                  </Typography>
+                </Paper>
+              </Button>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Button onClick={() => setshowchart(3)}>
+                <Paper
+                  elevation={3}
+                  sx={{
+                    maxWidth: "600px",
+                    padding: 2,
+                    color: "#041c49",
+                    backgroundColor: "#ffffff"
+                  }}
+                >
+                  <Typography variant="body1" display="block" align="center">
+                    project with least stages
+                  </Typography>
+                </Paper>
+              </Button>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Button onClick={() => setshowchart(4)}>
+                <Paper
+                  elevation={3}
+                  sx={{
+                    maxWidth: "600px",
+                    padding: 2,
+                    color: "#041c49",
+                    backgroundColor: "#ffffff"
+                  }}
+                >
+                  <Typography variant="body1" display="block" align="center">
+                    project with/without pipeline
+                  </Typography>
+                </Paper>
+              </Button>
+            </Grid>
+            <Grid item xs={4}>
+              <Button onClick={() => setshowchart(5)}>
+                <Paper
+                  elevation={3}
+                  sx={{
+                    maxWidth: "600px",
+                    padding: 2,
+                    color: "#041c49",
+                    backgroundColor: "#ffffff"
+                  }}
+                >
+                  <Typography variant="body1" display="block" align="center">
+                    Project last use pipeline
+                  </Typography>
+                </Paper>
+              </Button>
+            </Grid>
+          </Grid>
         </>
-      ) : (
-        <>
-          <Box sx={{ flexGrow: 1 }}>
-            <>
-              <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Grid item xs={4}>
-                  <Button onClick={() => setshowchart(1)}>
-                    <Paper
-                      elevation={3}
-                      sx={{
-                        maxWidth: "600px",
-                        padding: 2,
-                        color: "#041c49",
-                        backgroundColor: "#ffffff"
-                      }}
-                    >
-                      <Typography
-                        variant="body1"
-                        display="block"
-                        align="center"
-                      >
-                        Project branching strategy
-                      </Typography>
-                    </Paper>
-                  </Button>
-                </Grid>
-
-                <Grid item xs={4}>
-                  <Button onClick={() => setshowchart(2)}>
-                    <Paper
-                      elevation={3}
-                      sx={{
-                        maxWidth: "600px",
-                        padding: 2,
-                        color: "#041c49",
-                        backgroundColor: "#ffffff"
-                      }}
-                    >
-                      <Typography
-                        variant="body1"
-                        display="block"
-                        align="center"
-                      >
-                        Project failure rate/ success rate
-                      </Typography>
-                    </Paper>
-                  </Button>
-                </Grid>
-
-                <Grid item xs={4}>
-                  <Button onClick={() => setshowchart(3)}>
-                    <Paper
-                      elevation={3}
-                      sx={{
-                        maxWidth: "600px",
-                        padding: 2,
-                        color: "#041c49",
-                        backgroundColor: "#ffffff"
-                      }}
-                    >
-                      <Typography
-                        variant="body1"
-                        display="block"
-                        align="center"
-                      >
-                        project with least stages
-                      </Typography>
-                    </Paper>
-                  </Button>
-                </Grid>
-
-                <Grid item xs={4}>
-                  <Button onClick={() => setshowchart(4)}>
-                    <Paper
-                      elevation={3}
-                      sx={{
-                        maxWidth: "600px",
-                        padding: 2,
-                        color: "#041c49",
-                        backgroundColor: "#ffffff"
-                      }}
-                    >
-                      <Typography
-                        variant="body1"
-                        display="block"
-                        align="center"
-                      >
-                        project with/without pipeline
-                      </Typography>
-                    </Paper>
-                  </Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button onClick={() => setshowchart(5)}>
-                    <Paper
-                      elevation={3}
-                      sx={{
-                        maxWidth: "600px",
-                        padding: 2,
-                        color: "#041c49",
-                        backgroundColor: "#ffffff"
-                      }}
-                    >
-                      <Typography
-                        variant="body1"
-                        display="block"
-                        align="center"
-                      >
-                        Project last use pipeline
-                      </Typography>
-                    </Paper>
-                  </Button>
-                </Grid>
+      </Box>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="All Domain " value="1" />
+              <Tab label="Domain 1" value="2" />
+              <Tab label="Domain 2" value="3" />
+              <Tab label="Domain 3" value="4" />
+              <Tab label="Domain 4" value="5" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">
+            <Grid container>
+              <Grid item lg={4}>
+                {showchart === 1 ? (
+                  <LineRechartComponent />
+                ) : showchart === 2 ? (
+                  <PieComponent />
+                ) : showchart === 3 ? (
+                  <BarChartComponent />
+                ) : showchart === 4 ? (
+                  <CustomBarChart />
+                ) : showchart === 5 ? (
+                  <LineComponent />
+                ) : (
+                  "please select the card to view detaislss"
+                )}
+                {/* <ActiveShapePie /> */}
+                Services within Domain 1
               </Grid>
-            </>
+              <Grid item lg={1}></Grid>
+              <Grid item lg={6}>
+                1
+              </Grid>
+            </Grid>
+          </TabPanel>
+          <TabPanel value="2">Domain 2</TabPanel>
+          <TabPanel value="3">Domain 3</TabPanel>
+        </TabContext>
+      </Box>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Domain 1" value="1" />
+              <Tab label="Domain 2" value="2" />
+              <Tab label="Domain 3" value="3" />
+            </TabList>
           </Box>
-          <Box sx={{ width: "100%", typography: "body1" }}>
-            <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <TabList
-                  onChange={handleChange}
-                  aria-label="lab API tabs example"
-                >
-                  <Tab label="All Domain " value="1" />
-                  <Tab label="Domain 1" value="2" />
-                  <Tab label="Domain 2" value="3" />
-                  <Tab label="Domain 3" value="4" />
-                  <Tab label="Domain 4" value="5" />
-                </TabList>
-              </Box>
-              <TabPanel value="1">
-                <Grid container>
-                  <Grid item lg={4}>
-                    {showchart === 1 ? (
-                      <LineRechartComponent />
-                    ) : showchart === 2 ? (
-                      <PieComponent />
-                    ) : showchart === 3 ? (
-                      <BarChartComponent />
-                    ) : showchart === 4 ? (
-                      <CustomBarChart />
-                    ) : showchart === 5 ? (
-                      <LineComponent />
-                    ) : (
-                      "please select the card to view detaislss"
-                    )}
-                    {/* <ActiveShapePie /> */}
-                    Services within Domain 1
-                  </Grid>
-                  <Grid item lg={1}></Grid>
-                  <Grid item lg={6}>
-                    1
-                  </Grid>
-                </Grid>
-              </TabPanel>
-              <TabPanel value="2">Domain 2</TabPanel>
-              <TabPanel value="3">Domain 3</TabPanel>
-            </TabContext>
-          </Box>
-          <Box sx={{ width: "100%", typography: "body1" }}>
-            <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <TabList
-                  onChange={handleChange}
-                  aria-label="lab API tabs example"
-                >
-                  <Tab label="Domain 1" value="1" />
-                  <Tab label="Domain 2" value="2" />
-                  <Tab label="Domain 3" value="3" />
-                </TabList>
-              </Box>
-              <TabPanel value="1">
-                <Grid container>
-                  <Grid item lg={4}>
-                    <ActiveShapePie />
-                    Services within Domain 1
-                  </Grid>
-                  <Grid item lg={1}></Grid>
-                  <Grid item lg={6}>
-                    <EnhancedTableHead />
-                  </Grid>
-                </Grid>
-              </TabPanel>
-              <TabPanel value="2">Domain 2</TabPanel>
-              <TabPanel value="3">Domain 3</TabPanel>
-            </TabContext>
-          </Box>
-        </>
-      )}
+          <TabPanel value="1">
+            <Grid container>
+              <Grid item lg={4}>
+                <ActiveShapePie />
+                Services within Domain 1
+              </Grid>
+              <Grid item lg={1}></Grid>
+              <Grid item lg={6}>
+                <EnhancedTableHead />
+              </Grid>
+            </Grid>
+          </TabPanel>
+          <TabPanel value="2">Domain 2</TabPanel>
+          <TabPanel value="3">Domain 3</TabPanel>
+        </TabContext>
+      </Box>
     </>
   );
 }
